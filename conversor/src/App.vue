a4e2134f1f7f9b8839660a33d2d2d0d4a585c431
Brenno <brenno.anjos@souunit.com.br>
2020-06-06 04:13:44 -0300

    Subindo o projeto

---
+++ conversor/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1> Conversor de Moedas </h1>
+    <div class="linha">
+    <Conversor moeda-a="USD" moeda-b="BRL"/>
+    <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+    <Conversor moeda-a="EUR" moeda-b="BRL"/>
+    <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
+    <div class="linha">
+    <Conversor moeda-a="JPY" moeda-b="BRL"/>
+    <Conversor moeda-a="BRL" moeda-b="JPY"/>
+    </div>
+    <div class="linha">
+    <Conversor moeda-a="USD" moeda-b="JPY"/>
+    <Conversor moeda-a="JPY" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+    <Conversor moeda-a="USD" moeda-b="EUR"/>
+    <Conversor moeda-a="EUR" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+    <Conversor moeda-a="USD" moeda-b="GBP"/>
+    <Conversor moeda-a="GBP" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+    <Conversor moeda-a="BRL" moeda-b="GBP"/>
+    <Conversor moeda-a="GBP" moeda-b="BRL"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.linha{
+  display: flex;
+  justify-content: space-around;
+}
+</style>
